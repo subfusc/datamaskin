f09a36a74f88a79bf5e6688c36da315329747335
Sindre Wetjen <git@tjenen.de>
2019-11-14 22:54:10 +0100

    [Plugin] Working KosKarma

---
+++ plugins/KosKarma/__init__.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+import re
+import string
+from os import sep
+from os import path
+from os import makedirs
+from os.path import exists
+from .KosBackend import KosBackend
+from shutil import copytree
+from shutil import rmtree
+from shutil import move
+from os import getenv
+
+
+class Plugin(object):
+
+    def __init__(self, *args):
+        self.backends = dict()
+        self.prefix = "data" + sep + "karma"
+        self.suffix = ".karma"
+
+        if exists(self.prefix):
+            copytree(self.prefix, self.prefix + "backup")
+            rmtree(self.prefix)
+            move(self.prefix + "backup", self.prefix)
+
+        self.reg = re.compile(r"""
+        (\\addtocounter\{([^}]+)\}(\{(\d+)\})) # Latex Style \addtocounter{x}{y}
+        |(\(decf\s+([^\s()]+)\s*(\s+(\d+))?\)) # Lisp (decf x y) same as --
+        |(\(incf\s+([^\s()]+)\s*(\s+(\d+))?\)) # Lisp (incf x y) same as ++
+        |(\S+\+\+)|(\S+--)|(--\S+)|(\+\+\S+)   # Normal ++ -- C++-style add
+        """, re.U + re.X)
+
+        if not path.isdir(self.prefix):
+            makedirs(self.prefix)
+
+    def backend(self, chan):
+        chan = chan + self.suffix
+        if chan not in self.backends:
+            bpath = path.join(self.prefix, chan)
+            self.backends[chan] = KosBackend(bpath, 90, True)
+        return self.backends[chan]
+
+    def help(self, command, args, channel, **kwargs):
+        if command == "+1":
+            return [(1, kwargs["from_nick"], "!+1 [thing] gives karma to [thing]")]
+        if command == "-1":
+            return [(1, kwargs["from_nick"], "!-1 [thing] takes karma from [thing]")]
+        if command == "karma":
+            return [(1, kwargs["from_nick"], "!karma [thing] gives karma for [thing]")]
+        if command == "karmaprec":
+            return [(1, kwargs["from_nick"], "!karmaprec [thing] gives karma for [thing] with more precision")]
+        if command == "lskarma":
+            return [(1, kwargs["from_nick"], "!lskarma lists all karma things")]
+        if command == "highkarma":
+            return [(1, kwargs["from_nick"], "!hikarma [n] lists n best things")]
+        if command == "high":
+            return [(1, kwargs["from_nick"], "!high [n] lists n best things")]
+        if command == "lokarma":
+            return [(1, kwargs["from_nick"], "!lokarma [n] lists n worst things")]
+        if  command == "low":
+            return [(1, kwargs["from_nick"], "!low [n] lists n worst things")]
+        if command == "rmkarma":
+            return [(1, kwargs["from_nick"], "!rmkarma [thing] \"Deletes all karma for a given thing\"")]
+
+
+    def cmd(self, command, args, channel, **kwargs):
+        if command == "+1":
+            if args:
+                if args != kwargs['from_nick']:
+                    self.backend(channel).positiveKarma(to_unicode(args.strip()))
+            else:
+                return self.help(command, args, channel,**kwargs)
+
+        if command == "-1":
+            if args:
+                self.backend(channel).negativeKarma(to_unicode(args.strip()))
+            else:
+                return self.help(command, args, channel,**kwargs)
+
+        if command == "karma_en":
+            if args:
+                karma = self.backend(channel).getKarma(to_unicode(args.strip()))
+                ret = "{e} has karma: {k:.2f} with a total of {p} positive and {n} negative.".format(e = args, k = karma[0], p = karma[1], n = karma[2])
+                return [(1, channel, ret)]
+            else:
+                return self.help(command, args, channel, **kwargs)
+
+        if command == "karma":
+            if args:
+                karma = self.backend(channel).getKarma(to_unicode(args.strip()))
+                ret = "{e} har karma: {k:.1f} og har fått {p} positive og {n} negative.".format(e = args, k = karma[0], p = karma[1], n = karma[2])
+                return [(1, channel, ret)]
+            else:
+                return self.help(command, args, channel, **kwargs)
+
+        if command == "karmaprec":
+            if args:
+                karma = self.backend(channel).getKarma(to_unicode(args.strip()))
+                ret = "{e} has karma {k:.5f} with a total of {p} positive and {n} negative.".format(e = args, k = karma[0], p = karma[1], n = karma[2])
+                return [(1, channel, ret)]
+            else:
+                return self.help(command, args, channel, **kwargs)
+
+        if command == "hikarma" or command == "high":
+            if args:
+                if not args.isdigit():
+                    return self.help(command, args, channel, **kwargs)
+                best = self.backend(channel).getNBestList(int(args))
+            else:
+                best = self.backend(channel).getNBestList()
+            best = ', '.join(["{}: {:.2f}".format(e, k[0]) for e,k in best])
+            return [(1, channel, "good karma things in {} - {}".format(channel, best))]
+
+        if command == "høy" or command == "snill":
+            if args:
+                if not args.isdigit():
+                    return self.help(command, args, channel, **kwargs)
+                best = self.backend(channel).getNBestList(int(args))
+            else:
+                best = self.backend(channel).getNBestList()
+            best = ', '.join(["{}: {:.1f}".format(e, k[0]) for e,k in best])
+            return [(1, channel, "Bra karma i {}: {}".format(channel, best))]
+
+        if command == "lav" or command == "slem":
+            if args:
+                if not args.isdigit():
+                    return self.help(command, args, channel, **kwargs)
+                worst = self.backend(channel).getNWorstList(int(args))
+            else:
+                worst = self.backend(channel).getNWorstList()
+            worst = ', '.join(["{}: {:.1f}".format(e, k[0]) for e,k in worst])
+            return [(1, channel, "Dårlig karma i {}: {}".format(channel, worst))]
+
+        if command == "lokarma" or command == "low":
+            if args:
+                if not args.isdigit():
+                    return self.help(command, args, channel, **kwargs)
+                worst = self.backend(channel).getNWorstList(int(args))
+            else:
+                worst = self.backend(channel).getNWorstList()
+            worst = ', '.join(["{}: {:.2f}".format(e, k[0]) for e,k in worst])
+            return [(1, channel, "bad karma things in {} - {}".format(channel, worst))]
+
+        if command == "rmkarma":
+            args_split = args.split()
+            if kwargs['admin'] and len(args_split) >= 2:
+                self.backend(args_split[0]).delEntity(' '.join(args_split[1:]))
+
+    def listen(self, msg, channel, **kwargs):
+        for karmatoken in self.reg.findall(msg):
+            match = [x for x in karmatoken if x != ""]
+            if match[0].startswith("++") or match[0].endswith("++"):
+                if match[0].strip("++") != kwargs['from_nick']:
+                    self.backend(channel).positiveKarma(match[0].strip("++"))
+
+            if match[0].startswith("--") or match[0].endswith("--"):
+                self.backend(channel).negativeKarma(match[0].strip("--"))
+
+            if match[0].startswith("(incf") or match[0].startswith("\\addtocounter"):
+                if len(match) > 2:
+                    number = int(match[3])
+                    if number <= 3 and number > 0 and match[1] != kwargs['from_nick']:
+                        for i in range(1, number):
+                            self.backend(channel).positiveKarma(match[1])
+                    else:
+                        return [(1, kwargs['from_nick'],
+                                 "You are not allowed to give more than 3 karmas (and not under 1) at once.")]
+                if match[1] != kwargs['from_nick']:
+                    self.backend(channel).positiveKarma(match[1])
+
+            if match[0].startswith("(decf"):
+                if len(match) > 2:
+                    number = int(match[3])
+                    if number <= 3 and number > 0 and match[1] != kwargs['from_nick']:
+                        for i in range(1, number):
+                            self.backend(channel).negativeKarma(match[1])
+                    else:
+                        return [(1, kwargs['from_nick'],
+                                 "You are not allowed to take more than 3 karmas (and not under 1) at once.")]
+                if match[1] != kwargs['from_nick']:
+                    self.backend(channel).negativeKarma(match[1])
+
+    def stop(self):
+        for be in self.backends.values():
+            be.disconnect()
+
+
+if __name__ == '__main__':
+    k = Plugin()
+    k.listen('(decf microsoft)', '#foobar', from_nick='gitlab')
+    print(k.cmd('karmaprec', 'microsoft', '#foobar', from_nick='gitlab'))
+    print(k.cmd('rmkarma', '#foobar microsoft', '#foobar', from_nick='gitlab', admin=True))
+    print(k.cmd('karmaprec', 'microsoft', '#foobar', from_nick='gitlab'))
